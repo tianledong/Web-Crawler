20b7311d7fbfa1ec2a0e3a5fdaada86bf351dc04
Tianle Dong <dtllawrence@gmail.com>
2021-02-11 14:27:06 -0500

    done

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import socket
 import sys
-import re
 
-queue = []
-visited_urls = {}
+stack = []
+visited_urls = []
 flags = []
 
-LOGIN_URL = "http://webcrawler-site.ccs.neu.edu/accounts/login/?next=/fakebook/"
 HOST = "webcrawler-site.ccs.neu.edu"
 PORT = 80
-USERNAME = "dong.tia"
-PASSWORD = "CP2VSD48LYTFHSVA"
 DELIMITER = "\r\n\r\n"
+ROOT = '/fakebook/'
+FLAG_STR = "<h2 class='secret_flag'"
 
 
 def is_full_message(message):
@@ -23,7 +21,7 @@ def is_full_message(message):
 
 
 def send_recv_mes(message):
-    # Create socket normally
+    # Create socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # Connect to server
     try:
@@ -80,24 +78,115 @@ def login(username, password):
     send_str = '\r\n'.join(send_list)
     mes = send_recv_mes(send_str)
 
-    # find token in return message
+    # find session id in return message
     s_string = 'sessionid='
     token_index = mes.find(s_string) + len(s_string)
     other_index = mes.find(';', token_index, len(mes))
     session_id = mes[token_index:other_index]
-
-    print(token, session_id)
     return token, session_id
 
 
-def main():
-    token, session_id = login(USERNAME, PASSWORD)
+def get_url_301(header):
+    lst = header.split('\n')
+    keyword = 'Location: '
+    index = [i for i, s in enumerate(lst) if keyword in s]
+    location_line = lst[index[0]]
+    url = location_line[len(keyword):]
+    return url
 
-    # while len(queue) > 0:
-    #     link = queue.pop()
-    #     if link not in visited_urls:
-    #         pass
+
+def get_request(session_id, token, url):
+    # we make the host alway as webcrawler-site.ccs.neu.edu to make sure we wont go to other webs
+    send_list = [
+        f'GET {url} HTTP/1.1',
+        f'Host: {HOST}',
+        f'Cookie: csrftoken={token}; sessionid={session_id}',
+        '\r\n'
+    ]
+    send_str = '\r\n'.join(send_list)
+    res = send_recv_mes(send_str)
+    header, deli, body = res.partition(DELIMITER)
+    status = get_header_status(header)
+    # success
+    if status == '200':
+        return body
+    # redirect status. try new url
+    elif status == '301':
+        new_url = get_url_301(header)
+        return get_request(session_id, token, new_url)
+        # retry the request to this url until success
+    elif status == '500':
+        return get_request(session_id, token, url)
+    # when status == '403' or status == '404' or other. Abandon url
+    else:
+        return
+
+
+def get_header_status(header):
+    header = header.split('\n')[0]
+    status = header.split(' ')[1]
+    return status
+
+
+def search_flag(body):
+    # as the syntax of the flag exactly follow the format
+    index = body.find(FLAG_STR)
+    if index != -1:
+        # the length of the previous html is 47 long
+        index = index + 48
+        # the flag is exactly 64 long
+        flag = body[index:index + 64]
+        print(flag)
+        return flag
+
+
+def find_url_in_body(body):
+    index = body.find('<div id="content">')
+    body_len = len(body)
+    while index < body_len:
+        index = body.find('href="', index, body_len)
+        if index == -1:
+            return
+        index += len('href="')
+        back_index = body.find('"', index, body_len)
+        url = body[index:back_index]
+        if url not in stack and url not in visited_urls:
+            stack.append(url)
+        index = back_index
+
+
+def crawl(token, session_id, root_url):
+    stack.append(root_url)
+    while len(stack) > 0:
+        url = stack.pop()
+        visited_urls.append(url)
+        # make a get request to get data from this url
+        body = get_request(session_id, token, url)
+        if body:
+            flag = search_flag(body)
+            if flag:
+                flags.append(flag)
+                # when find all flags break the loop
+                if len(flags) > 4:
+                    print('\n'.join(flags))
+                    return
+            find_url_in_body(body)
+
+
+def main(args):
+    # USERNAME = "dong.tia"
+    # PASSWORD = "CP2VSD48LYTFHSVA"
+
+    if len(args) > 1:
+        username = args[1]
+        password = args[2]
+    else:
+        print("Invalid Inputs")
+        raise SystemExit
+
+    token, session_id = login(username, password)
+    crawl(token, session_id, ROOT)
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
